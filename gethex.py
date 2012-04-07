b007592f4ffdb1d7089b39a532f88cff957d0d76
Sam Phippen <samphippen@googlemail.com>
2012-04-07 18:34:35 +0100

    Initial commit
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

---
+++ gethex.py
@@ -0,0 +1,34 @@
+import json
+import urllib
+
+def ishexonly(string):
+    hex = "abcdef0123456789ABCDEF"
+    for x in string:
+        if x not in hex:
+            return False
+    return True
+
+def get_hexes():
+    string = ""
+    #open the reddit thread
+    json_obj = json.loads(urllib.urlopen("http://www.reddit.com/r/programming/comments/rxve9/let_us_have_hex/.json").read())
+
+    #put the comments in a giant string
+    for comment in json_obj[1]["data"]["children"]:
+        string += comment["data"]["body"] + "\n"
+
+    hexes = []
+
+    #find all the hex strings in that string
+    for x in string.split("\n"):
+        if ishexonly(x) and x != "":
+            if x not in hexes:
+                hexes.append(x)
+
+    return hexes
+
+if __name__ == "__main__":
+    #print all the data
+    for x in get_hexes():
+        print x
+
---
+++ readme.md
@@ -0,0 +1,4 @@
+#gethex
+
+This is a script designed to accumulate all the results posted in this reddit thread:
+http://www.reddit.com/r/programming/comments/rxve9/let_us_have_hex/
